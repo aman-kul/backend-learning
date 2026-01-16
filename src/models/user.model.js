eded72fb63ba32ceeab971758ae6c06d48281a05
aman.kulshrestha <aman.k@compliancekart.io>
2026-01-16 11:04:12 +0530

    update user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -48,4 +48,39 @@ const userSchema = new Schema({
 
 }, { timestamps: true })
 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.method.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.method.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.method.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id
+    },
+        process.env.REFRESH_TOKEB_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const user = mongoose.model('user', userSchema)
