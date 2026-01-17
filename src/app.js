24b8584b66ff64bca3fa8daa1f2ce256f94c8167
aman.kulshrestha <aman.k@compliancekart.io>
2026-01-17 18:01:20 +0530

    complete the config

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import cors from cors
+import cors from 'cors'
 
 const app = express();
 
@@ -12,6 +12,13 @@ app.use(cors({
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cookieParser());
+
+
+// routes
+
+import userRoutes from './routes/user.routes.js'
+
+app.use('/api/v1/users', userRoutes)
 
 export default app;
